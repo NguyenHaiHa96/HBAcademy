ec6c22e4e9a76e6e274a1ad6215f13394a7dda12
NguyenHaiHa96 <96693278+NguyenHaiHa96@users.noreply.github.com>
2023-03-07 14:46:06 +0700

    + Packages, Particle ~ Random name

---
+++ Assets/GameAssets/Scripts/Constant.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constant 
+{
+    #region Variables
+    public static readonly Vector2 SizeDelta200 = new Vector2(200, 200);
+    public static readonly Vector2 SizeDelta350 = new Vector2(350, 350);
+    public static readonly Vector3 StartScaleSize3 = new Vector3(3, 3, 3);
+    public static readonly Vector3 ScaleUpSize12 = new Vector3(1.2f, 1.2f, 1.2f);
+    public static readonly Vector3 ScaleDownSize08 = new Vector3(0.8f, 0.8f, 0.8f);
+    public static readonly float TweenOnShowDuration1 = 1f;
+    public static readonly float TweenDuration05 = 0.5f;
+    public static readonly float TweenDuration034 = 0.34f;
+    public static readonly float TweenDuration02 = 0.2f;
+    public static readonly float TweenDuration01 = 0.1f;
+    #endregion
+}
--- Assets/GameAssets/Scripts/RandomName.cs
+++ Assets/GameAssets/Scripts/RandomName.cs
@@ -6,13 +6,18 @@ using TMPro;
 
 public class RandomName : MonoBehaviour
 {
+    public ParticleSystem FX;
     public List<string> Names = new List<string>();
     public Button RandomNameButton;
     public TextMeshProUGUI NameText;
+    public float TimeGetName = 0.2f;
+    public float DelayTime;
+    public float DelayFX = 0.4f;
 
     private void Start()
     {
         RandomNameButton.onClick.AddListener(GetRandomName);
+        Helper.ButtonOnClickTween(RandomNameButton);    
     }
 
     private void GetRandomName()
@@ -20,8 +25,22 @@ public class RandomName : MonoBehaviour
         if (Names.Count == 0) return;
         int randomIndex = 0;
         randomIndex = Random.Range(0, Names.Count - 1);
-        NameText.SetText(Names[randomIndex]);
-        Names.RemoveAt(randomIndex);
+        StartCoroutine(DelayGetRandomName(randomIndex)); 
+    }
 
+    private IEnumerator DelayGetRandomName(int index)
+    {
+        int randomIndex = 0;
+        for (int i = 0; i < 10; ++i)
+        {
+            randomIndex = Random.Range(0, Names.Count - 1);
+            NameText.SetText(Names[randomIndex]);
+            yield return Helper.GetWaitForSeconds(TimeGetName);
+        }
+        yield return new WaitForEndOfFrame();
+        NameText.SetText(Names[index]);
+        Names.RemoveAt(index);
+        yield return Helper.GetWaitForSeconds(DelayFX);
+        FX.Play();
     }
 }
